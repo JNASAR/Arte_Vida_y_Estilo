4709334d99bc2afd62c85d5606c42db85d46365e
JNASAR <64946335+JNASAR@users.noreply.github.com>
2024-04-21 21:12:12 -0500

    Add files via upload

---
+++ Arte Vida y Estilo/AVE/Logica1.js
@@ -0,0 +1,10 @@
+// script.js
+
+let carrito = []; // Array para almacenar los productos en el carrito
+
+function agregarAlCarrito(idProducto) {
+    // Aquí podrías implementar lógica para agregar el producto al carrito
+    // Por ejemplo, podrías guardar el ID del producto en el array del carrito
+    carrito.push(idProducto);
+    alert("Producto agregado al carrito");
+}
